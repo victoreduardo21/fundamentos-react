96c30007e9e0c79570d496aad12fee763d1a18eb
victor eduardo <eduardopontesdias@outlook.com>
2024-02-26 20:23:32 -0300

    componentes de formularios

--- src/components/formulario/input.jsx
+++ src/components/formulario/input.jsx
@@ -1,12 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import './input.css'
 
 export default props => {
 
-    const [valor, setValor] = useState ('inicial')
+    const [valor, setValor] = useState('inicial')
+
+    function quandoMudas(e) {
+        setValor(e.target.value)
+    }
+
 
     return (
-        <div>
-            input
+        <div className="input">
+            <h3>{valor}</h3>
+            <input value={valor}  onChange={quandoMudas}/>
         </div>
 
     )
