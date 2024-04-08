4226a6127ac58a5b28ca9011ed94c5d07b6f4241
victor eduardo <eduardopontesdias@outlook.com>
2024-04-07 21:18:38 -0300

    contador parte 2

--- src/components/Contador/contador.jsx
+++ src/components/Contador/contador.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import"./contador.css"
+import Display from "./display"
+import Botoes from "./botoes"
+import PassoForm from "./passoForm"
+import "./contador.css"
 
 
 class Contador extends Component {
@@ -23,9 +26,9 @@ class Contador extends Component {
         })
     }
 
-    setPasso = (evento) => {
+    setPasso = (novoPasso) => {
         this.setState({
-            passo: + evento.target.value
+            passo: novoPasso,
         })
     }
 
@@ -35,13 +38,10 @@ class Contador extends Component {
         return (
             <div className="contador">
                 <h2>Contador</h2>
-                <h3>{this.state.numero}</h3>
-                <div>
-                    <label htmlFor="passoInput">Passo: </label>
-                    <input id="passoInput" type="number" value={this.state.passo} onChange={this.setPasso} />
-                </div>
-                <button onClick={this.inc}>+</button>
-                <button onClick={this.dec}>-</button>
+                <Display numero={this.state.numero} />
+                <PassoForm passo={this.state.passo} setPasso={this.setPasso}/>
+                <Botoes dec={this.dec} inc={this.inc} />
+
 
             </div>
         )
